90ff815307b06b2cb258915cc5bd8df07d8de3f8
Иван Батраков <batrakov.id@reliab.tech>
2024-08-04 19:46:40 +0300

    Initial commit

---
+++ static/scripts.js
@@ -0,0 +1,81 @@
+function checkboxClicked(checkbox) {
+    const status = checkbox.checked ? 'gold sticker' : 'no gold sticker';
+
+    fetch('/change_joker_status', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            joker_name: checkbox.name,
+            status: status
+        })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Response from backend was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Success:', data);
+        changeJokerStats(checkbox.checked)
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        alert('Error, check the console')
+    });
+}
+
+function changeJokerStats(isChecked) {
+    const statsElement = document.getElementById("stats");
+    const progressElement = document.getElementById('progress');
+
+    let statsStr = statsElement.textContent;
+
+    const regex = /(\d+)\/\d+ \((\d+\.\d+)%\)/;
+    const match = statsStr.match(regex);
+
+    if (match) {
+        let currentStickers = parseInt(match[1], 10);
+        const totalStickers = parseInt(match[0].split('/')[1], 10);
+
+        if (isChecked) {
+            currentStickers += 1;
+        } else {
+            currentStickers -= 1;
+        }
+
+        const newPercentage = ((currentStickers / totalStickers) * 100).toFixed(2);
+
+        statsElement.textContent = currentStickers + '/' + totalStickers + ' (' + newPercentage + '%)';
+        progressElement.value = newPercentage;
+
+        console.log("Updated Stats:", statsElement.textContent);
+    } else {
+        console.log("No match found.");
+    }
+}
+
+function handleButtonClick(url) {
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Response from backend was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Success:', data);
+        window.location.reload();
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        alert('Error, check the console')
+   });
+}
